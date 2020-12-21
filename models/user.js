9c4700a347a894feb6faf53e33171acd59693d29
Zoe <heejeong.cho@stryx.co.kr>
2020-12-21 14:11:57 +0900

    connect mongoDB

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new mongoose.Schema({
+    _id: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        auto: true,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+},
+{timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
